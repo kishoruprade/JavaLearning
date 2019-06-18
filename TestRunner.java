b517e3024c5a68105c074acd91305aaaf48f659d
kishoruprade <49707800+kishoruprade@users.noreply.github.com>
2019-06-18 11:32:39 +0530

    git commit - m "BDD and Appium"

--- TestRunner.java
+++ TestRunner.java
@@ -1,14 +1,20 @@
-package Runner;
+package runner;
 
 import org.junit.runner.RunWith;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"C:\\Users\\UpradeK\\workspace\\MercuryBDDFrmWrk\\src\\main\\java\\Feature"},
-               glue={"StepDefinition"}
+@CucumberOptions(features={"C:\\Users\\UpradeK\\workspace\\MercuryToursBDDFrameWork\\src\\main\\java\\featureFile"},
+               glue={"stepDefination"}
                            		)
 
+
 public class TestRunner {
+	
+
+
+	
 
 }
---
+++ searchflight.feature
@@ -0,0 +1,7 @@
+Feature: Mercury User login
+
+Scenario: Mercury User Login test Scenario
+
+Given: User is already on login page
+When:  Title of login page is Mercury Login
+Then: user enters username and enters password
